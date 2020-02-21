5ad7285f148b08bd1945a9b17d7eeef3c4c12b7d
Lucas <lucassmi33@hotmail.fr>
2020-02-21 15:21:21 +0100

    version 0.2 sh

--- script_hbase.sh
+++ script_hbase.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Création table CUSTOMER et familles associées
 echo "create 'CUSTOMER', {NAME=> 'civ', VERSIONS=>2}" | hbase shell -n
 status_code=$?
 if [ ${status_code} -ne 0 ]; then
@@ -16,4 +17,17 @@ echo "alter 'CUSTOMER', {NAME=> 'add', VERSIONS=>2}" | hbase shell -n
 status_code=$?
 if [ ${status_code} -ne 0 ]; then
   echo "The command alter table CUSTOMER and family add may have failed."
+fi
+
+# Création table FILM et familles associées
+echo "create 'FILM', {NAME=> 'mov', VERSIONS=>1}" | hbase shell -n
+status_code=$?
+if [ ${status_code} -ne 0 ]; then
+  echo "The create table FILM and family mov may have failed."
+fi
+
+echo "alter 'FILM', {NAME=> 'dis', VERSIONS=>1}" | hbase shell -n
+status_code=$?
+if [ ${status_code} -ne 0 ]; then
+  echo "The command alter table FILM and family dis may have failed."
 fi
